60052a79b44afab5127d79d8c3c86f4a4d5b0273
Skyebud <143579233+Skyebud@users.noreply.github.com>
2026-07-26 19:26:29 -0500

    Updae Firebase config.js 2.0
    0.3.3

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Firebase Console > Project settings > Your apps > Web app
+export const firebaseConfig = {
   apiKey: "AIzaSyDTVbt8VE4-xDLdiROvNnBMfbJXXErnxmo",
   authDomain: "lita-bdc85.firebaseapp.com",
   projectId: "lita-bdc85",
@@ -12,6 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "729329565710",
   appId: "1:729329565710:web:5541d27d98f8cf494e7c5f"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
